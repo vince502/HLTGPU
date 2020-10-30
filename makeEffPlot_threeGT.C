31625a453e7637a6d33c7318c24c0e3261ac4e58
Soohwan Lee <soohwan.lee@cern.ch>
2020-10-30 17:49:51 +0900

    new repo

---
+++ makeEffPlot_threeGT.C
@@ -0,0 +1,201 @@
+#include <iostream>
+#include "Style_jaebeom.h"
+#include <stdio.h>
+#include <string.h>
+
+
+using namespace std;
+
+double getErrorPropaDivide(double a, double da, double b, double db){
+  double err = a/b*TMath::Sqrt((da/a)*(da/a) + (db/b)*(db/b));
+  return err;
+}
+
+
+void makeEffPlot_threeGT(TString st_name = "HLT_HIL3DoubleMuOpen_v1", bool isPt=true, bool setLogopt = false, double ymax = 1.09, double ymin =0.91){
+  int coco =0;
+  setTDRStyle();
+  writeExtraText = true;
+  int iPeriod = 22;
+  int iPos = 33;
+
+  double posx1 = 0.5;
+  double posx2 = 0.7;
+  double posy1 = 0.78;
+  double posy2 = 0.90;
+ //std::cout<< "TEST_________________________________________________TEST, N:"<<coco<< std::endl; coco++; 
+  string ResDirN = "EffPlotRes_three"; 
+  const char* varn[3] = {"p_{T} (GeV/c)", "#eta", "y"};
+
+  TFile* f1 = new TFile("outputEff.root","read");
+  TList* listofkeys = f1->GetListOfKeys();
+  for(int key=0; key < listofkeys->GetEntries(); key++){
+    TString st_name = listofkeys->At(key)->GetName();
+//  TFile* f2 = new TFile("CALO_Track_miscal_Eff_v1.root","read");
+    TString dirName = st_name;
+/*  TString dirName = "";
+  if(strstr(st_name.Data(),"GED")){
+    if(isPt){dirName = "effSinglePhoton_" + st_name + "_Pt";}
+    else if(!isPt){dirName = "effSinglePhoton_" + st_name + "_Eta";}
+  }
+  else if(strstr(st_name.Data(),"DoubleMu")){
+    if(isPt){dirName = "effDoubleMuon_" + st_name + "_Pt";}
+    else if(!isPt){dirName = "effDoubleMuon_" + st_name + "_Rapidity";}
+  }
+  else if(strstr(st_name.Data(),"L3Mu")){
+    if(isPt){dirName = "effSingleMuon_" + st_name + "_Pt";}
+    else if(!isPt){dirName = "effSingleMuon_" + st_name + "_Eta";}
+  }
+  else if(strstr(st_name.Data(),"Gsf")){
+    if(isPt){dirName = "effSingleElectron_" + st_name + "_Pt";}
+    else if(!isPt){dirName = "effSingleElectron_" + st_name + "_Eta";}
+  } */
+
+
+  TGraphAsymmErrors* gonl = (TGraphAsymmErrors*) f1 -> Get(Form("%s/%s_online",dirName.Data(),dirName.Data()));
+  TGraphAsymmErrors* gtrk = (TGraphAsymmErrors*) f1 -> Get(Form("%s/%s_CPU_Pat",dirName.Data(),dirName.Data()));
+  TGraphAsymmErrors* gtrkcal = (TGraphAsymmErrors*) f1 -> Get(Form("%s/%s_GPU",dirName.Data(),dirName.Data()));
+
+  SetGraphStyle(gonl, 0, 0);
+  SetGraphStyle(gtrk, 1, 1);
+  SetGraphStyle(gtrkcal, 2, 2);
+
+  TGraphAsymmErrors* grtrk = new TGraphAsymmErrors();
+  TGraphAsymmErrors* grtrkcal = new TGraphAsymmErrors();
+        
+  for(int ip=0; ip<gonl->GetN(); ip++)
+  {
+    double px,py,px_,py_,ex,eyh,eyl,ex_,eyh_,eyl_;
+    gonl->GetPoint(ip,px,py);
+    gtrk->GetPoint(ip,px_,py_);
+
+    ex = gonl->GetErrorXlow(ip);
+    eyh = gonl->GetErrorYhigh(ip);
+    eyl = gonl->GetErrorYlow(ip);
+    ex_ = gtrk->GetErrorXlow(ip);
+    eyh_ = gtrk->GetErrorYhigh(ip);
+    eyl_ = gtrk->GetErrorYlow(ip);
+
+
+    double eyl_r = getErrorPropaDivide(py,eyl,py_,eyl_);
+    double eyh_r = getErrorPropaDivide(py,eyl,py_,eyl_);
+    double pr = py/py_;
+    grtrk->SetPoint(ip,px,pr);
+    grtrk->SetPointError(ip,ex,ex,eyl_r,eyh_r);
+  }
+
+ std::cout<< "TEST_________________________________________________TEST, N:"<<coco<< std::endl; coco++; 
+  for(int ip=0; ip<gonl->GetN(); ip++)
+  {
+    double px,py,px_,py_,ex,eyh,eyl,ex_,eyh_,eyl_;
+    gonl->GetPoint(ip,px,py);
+    gtrkcal->GetPoint(ip,px_,py_);
+
+    ex = gonl->GetErrorXlow(ip);
+    eyh = gonl->GetErrorYhigh(ip);
+    eyl = gonl->GetErrorYlow(ip);
+    ex_ = gtrkcal->GetErrorXlow(ip);
+    eyh_ = gtrkcal->GetErrorYhigh(ip);
+    eyl_ = gtrkcal->GetErrorYlow(ip);
+
+
+    double eyl_r = getErrorPropaDivide(py,eyl,py_,eyl_);
+    double eyh_r = getErrorPropaDivide(py,eyl,py_,eyl_);
+    double pr = py/py_;
+    grtrkcal->SetPoint(ip,px,pr);
+    grtrkcal->SetPointError(ip,ex,ex,eyl_r,eyh_r);
+  }
+  SetGraphStyle(grtrk,1, 1);
+  SetGraphStyle(grtrkcal, 2, 2);
+  grtrk->SetMarkerSize(0.8);
+  grtrkcal->SetMarkerSize(1.2);
+
+  TCanvas* c1 = new TCanvas("c1","",700,700);
+  c1->cd();
+
+  gonl->GetXaxis()->SetTitle(" ");
+  gonl->GetXaxis()->SetTitleSize(0);
+  gonl->GetXaxis()->SetLabelSize(0);
+  gonl->GetYaxis()->SetTitleOffset(0.9);
+  gonl->GetYaxis()->SetTitleSize(0.07);
+  gonl->GetYaxis()->SetLabelSize(0.06);
+  
+  gonl->GetXaxis()->SetNdivisions(510);
+
+  grtrk->GetXaxis()->SetNdivisions(510);
+  grtrk->GetYaxis()->SetLimits(ymin,ymax);
+  grtrk->GetYaxis()->SetRangeUser(ymin,ymax);
+  grtrk->GetXaxis()->SetTitleOffset(1.2);
+  grtrk->GetXaxis()->SetTitleSize(0.1);
+  grtrk->GetYaxis()->SetTitleOffset(0.62);
+  grtrk->GetYaxis()->SetTitleSize(0.1);
+  grtrk->GetXaxis()->SetLabelSize(0.1);
+  grtrk->GetYaxis()->SetLabelSize(0.085);
+  grtrk->GetYaxis()->SetNdivisions(505);
+  grtrk->GetYaxis()->SetTickSize(0.04);
+  grtrk->GetXaxis()->SetTickSize(0.06);
+  grtrk->GetXaxis()->SetTitle("p_{T} (GeV/c)");
+  grtrk->GetYaxis()->SetTitle("#frac{Online(CPU)}{CPUPATATRACK|GPU}");
+  grtrk->GetXaxis()->SetLimits(gonl->GetXaxis()->GetXmin(),gonl->GetXaxis()->GetXmax());
+
+  TPad* pad1 = new TPad("pad1", "",0, 0.4, 1.0, 1.0); 
+//  pad1->SetRightMargin(0.05);
+  pad1->SetTopMargin(0.090);
+  pad1->SetBottomMargin(0);
+  pad1->SetLeftMargin(0.14);
+  
+
+  TPad* pad2 = new TPad("pad2","",0, 0, 1.0,0.4);
+  pad2->SetTopMargin(0);
+  pad2->SetBottomMargin(0.3);
+  pad2->SetLeftMargin(0.14);
+//  pad2->SetRightMargin(0.05);
+
+
+  c1->cd();
+  pad1->Draw();
+  pad1->cd();
+  if(setLogopt)pad1->SetLogy();
+  gonl->Draw("AP");
+  gtrk->Draw("P");
+  gtrkcal->Draw("P");
+  TLegend* leg = new TLegend(posx1, posy1, posx2, posy2);
+  SetLegendStyle(leg);
+  leg->AddEntry(gonl,"CPU","pe");
+  leg->AddEntry(gtrk,"CPU_Patatrack","pe");
+  leg->AddEntry(gtrkcal,"GPU","pe");
+  leg->Draw("same"); 
+  drawGlobText(st_name.Data(), 0.2,0.55,1,0.034);
+        
+  
+  c1->cd();
+  pad2->Draw();
+  pad2->cd();
+  grtrk->Draw("AP");
+  grtrkcal->Draw("P");
+  dashedLine(gonl->GetXaxis()->GetXmin(),1,gonl->GetXaxis()->GetXmax(),1,1,1);
+//  grtrk->SetName(Form("%s",st_name.c_str())); 
+  pad2->Update();
+  c1->Modified();
+  c1->Update();
+  
+
+  pad1->Update();
+  c1->Update();
+
+
+  CMS_lumi_square( pad1, iPeriod, iPos,1);
+  c1->Modified();
+  c1->Update();
+
+  string savedir = ResDirN+"/"+st_name.Data();
+  void * dirf = gSystem->OpenDirectory(savedir.c_str());
+  if(dirf) gSystem->FreeDirectory(dirf);
+  else {gSystem->mkdir(savedir.c_str(), kTRUE);}
+
+  string pdfFileName = gonl->GetName();
+  const string pdfsave = pdfFileName.substr(0, pdfFileName.size()-7);
+
+  c1->SaveAs(Form("%s/%s.pdf",savedir.c_str(),pdfsave.c_str()));
+}
+}
---
+++ plotTurnOn_withsaved_MB.C
@@ -0,0 +1,256 @@
+#include "plotTurnOn.h"
+
+void makeDirFile(TFile *f1, const std::string& dir)
+{
+  TDirectory* subdir = f1->mkdir(dir.c_str());
+};
+
+void plotTurnOn_withsaved_MB()
+{
+  const std::string recFilePath = "../store/HiForestAOD_MB326381.root";
+  const std::vector<std::pair<std::string, std::string> > hltFilePath = {
+      {"CPU", "./HLTTrees/HLTTree_MB_CPU.root"},
+      {"CPU_Patatrack","./HLTTrees/HLTTree_MB_CPU_Patatrack.root"},
+      {"GPU","./HLTTrees/HLTTree_MB_GPU.root"}
+  };
+  const std::map<std::string, int> COLOR = { {"CPU", kBlack}, {"CPU_Patatrack", kRed}, {"GPU", kBlue} };
+
+  // initialize efficiency objects
+  TH1::AddDirectory(kFALSE);
+  std::vector<TEfficiency> effVec;
+  std::map<std::string, std::map<std::string, std::map<std::string, std::string> > > effMapTitle;
+  std::map<std::string, std::map<std::string, std::map<std::string, std::map<std::string, size_t> > > > effMapIdx;
+  for (const auto& p : TRIGLIST) {
+    for (const auto& t : p.second) {
+      for (const auto& b : BINMAP.at(p.first)) {
+        const std::string name = "eff"+p.first+"_"+t+"_"+b.first;
+        effMapTitle[p.first][t][b.first] = name;
+        for (const auto& c : hltFilePath) {
+          effVec.emplace_back(TEfficiency("", "", b.second.size()-1, b.second.data()));
+          effVec.back().SetName((name+"_"+c.first).c_str());
+          effMapIdx[p.first][t][b.first][c.first] = effVec.size()-1;
+        }
+      }
+    }
+  }
+
+  std::set<std::string> doParticle;
+  for (const auto& t : TRIGLIST) {
+    if      (t.first.rfind("Electron")!=std::string::npos) { doParticle.insert("electron"); }
+    else if (t.first.rfind("Photon"  )!=std::string::npos) { doParticle.insert("photon");   }
+    else if (t.first.rfind("Muon"    )!=std::string::npos) { doParticle.insert("muon");     }
+  }
+
+  // prepare multi-threading
+  const int nCores = doParticle.size()*hltFilePath.size();
+  ROOT::EnableImplicitMT();
+  ROOT::TProcessExecutor mpe(nCores);
+
+  TH1::AddDirectory(kFALSE);
+  auto fillEfficiency = [=](int idx)
+  {
+    auto c_start = std::clock();
+    const auto& n = doParticle.size();
+    const auto& hltFileP = hltFilePath[idx/n];
+    const auto& pTag = *std::next(doParticle.begin(), idx%n);
+    auto parName = pTag; parName[0] = toupper(parName[0]);
+
+    std::set<int> idxS;
+    std::vector<TEfficiency> effV(effVec);
+
+    // get offline
+    RecoReader recoInfo(recFilePath, false);
+
+    // get online information
+    TriggerReader triggerInfo(hltFileP.second, true);
+
+    // add trigger paths to trigger reader
+    for (const auto& t : TRIGLIST) {
+      for (const auto& path : t.second) {
+        triggerInfo.addTrigger(path);
+      }
+    }
+
+    // initialize offline information
+    recoInfo.initBranches(pTag);
+
+    // fill efficiencies
+    const auto nEntries = recoInfo.getEntries();
+    for (const auto& iEntry : ROOT::TSeqUL(nEntries)) {
+      if ((iEntry%100000)==0) { std::cout << "[INFO] Core " << idx << ":  Processing event " << iEntry << " / " << nEntries << std::endl; }
+      recoInfo.setEntry(iEntry, false, true);
+      if (!triggerInfo.setEntry(recoInfo.getEventNumber(), false, true)) continue;
+      // check that event pass event selection
+      if (!recoInfo.passEventSelection()) continue;
+      //const auto cent = recoInfo.getCentrality();
+      // loop over particle type
+      const auto particles = recoInfo.getParticles(pTag);
+      // loop over single particles
+      for (size_t iPar1=0; iPar1<particles.size(); iPar1++) {
+        const auto& particle1 = particles[iPar1];
+        // extract variables
+        const auto var = std::map<std::string, double>({ {"Pt",  particle1.first.Pt()}, {"Eta", particle1.first.Eta()} });
+        // loop over single particle triggers
+        auto parTag = "Single"+parName;
+        if (TRIGLIST.find(parTag)!=TRIGLIST.end()) {
+          for (const auto& path : TRIGLIST.at(parTag)) {
+            // check if trigger matched
+            const auto isMatched = triggerInfo.isTriggerMatched(particle1.first, path);
+            // fill efficiency
+            for (const auto& v : BINMAP.at(parTag)) {
+              const auto& index = effMapIdx.at(parTag).at(path).at(v.first).at(hltFileP.first);
+              effV[index].Fill(isMatched, var.at(v.first));
+              idxS.insert(index);
+            }
+          }
+        }
+        // loop over double particles
+        parTag = "Double"+parName;
+        if (TRIGLIST.find(parTag)!=TRIGLIST.end()) {
+          for (size_t iPar2=iPar1+1; iPar2<particles.size(); iPar2++) {
+            const auto& particle2 = particles[iPar2];
+            const auto p4 = particle1.first + particle2.first;
+            // extract variables
+            const auto varDP = std::map<std::string, double>({ {"Pt",  p4.Pt()}, {"Rapidity", p4.Rapidity()} });
+            // loop over ddouble particle triggers
+            for (const auto& path : TRIGLIST.at(parTag)) {
+              // check if trigger matched
+              const bool isMatched1 = triggerInfo.isTriggerMatched(particle1.first, path);
+              const bool isMatched2 = triggerInfo.isTriggerMatched(particle2.first, path);
+              const bool isMatched = isMatched1 && isMatched2;
+              // fill efficiency
+              for (const auto& v : BINMAP.at(parTag)) {
+                const auto& index = effMapIdx.at(parTag).at(path).at(v.first).at(hltFileP.first);
+                effV[index].Fill(isMatched, varDP.at(v.first));
+                idxS.insert(index);
+              }
+            }
+          }
+        }
+      }
+    }
+    return std::make_pair(idxS, effV);
+  };
+
+  const auto& res = mpe.Map(fillEfficiency, ROOT::TSeqI(nCores));
+
+  for (const auto& r : res) {
+    for (const auto& idx : r.first) {
+      effVec[idx] = r.second[idx];
+    }
+  }
+
+  // set plot style
+  setTDRStyle();
+  gStyle->SetOptStat(0);
+  gStyle->SetOptFit(0);
+
+  TFile *file = new TFile("outputEff.root","recreate");
+
+  // plot efficiencies
+  for (const auto& p : effMapIdx) {
+    for (const auto& t : p.second) {
+      for (const auto& v : t.second) {
+        const auto& par = p.first;
+        const auto& path = t.first;
+        const auto& var = v.first;
+        const auto& idxM = v.second;
+        // create Canvas
+        TCanvas c("c", "c", 1000, 1000); c.cd();
+        // create the text info
+        TLatex tex; tex.SetNDC(); tex.SetTextSize(0.028); float dy = 0;
+        std::vector< std::string > textToPrint;
+        textToPrint.push_back(path);
+        if (par.rfind("Electron")!=std::string::npos) {
+          textToPrint.push_back("p^{e}_{T} > 20 GeV/c");
+          textToPrint.push_back("|#eta^{e}| < 2.1");
+        }
+        else if (par.rfind("Photon")!=std::string::npos) {
+          textToPrint.push_back("p^{#gamma}_{T} > 40 GeV/c");
+          textToPrint.push_back("|#eta^{e}| < 2.4");
+        }
+        else if (par.rfind("Muon")!=std::string::npos) {
+          textToPrint.push_back("p^{#mu}_{T} > 1.5 GeV/c");
+          textToPrint.push_back("|#eta^{#mu}| < 2.4");
+        }
+        // format efficiency
+        std::vector<std::pair<std::string, TGraphAsymmErrors> > graphM;
+	int count =0;
+        for (const auto& c : hltFilePath) {
+          auto& eff = effVec[idxM.at(c.first)];
+          eff.Draw(); gPad->Update();
+          graphM.push_back({c.first, *eff.GetPaintedGraph()});
+          auto& graph = graphM.back().second;
+          graph.SetName(eff.GetName());
+          formatEff(graph, par, var);
+          graph.SetMarkerColor(COLOR.at(c.first));
+          graph.SetLineColor(COLOR.at(c.first));
+          graph.GetXaxis()->SetLimits(eff.GetTotalHistogram()->GetXaxis()->GetXmin(), eff.GetTotalHistogram()->GetXaxis()->GetXmax());
+          if (c.first!=hltFilePath.begin()->first) {
+            graph.SetMarkerSize(2);
+	    graph.SetMarkerStyle(count+20);
+	    graph.SetLineColorAlpha(COLOR.at(c.first),1-0.3*count);
+            graph.SetLineWidth((int) 3*(count));
+          }
+	  count++;
+        }
+        auto& graph = graphM[0].second;
+        if (graph.GetN()==0) continue;
+        // add to legend
+        TLegend leg(0.43, 0.74, 0.6, 0.84);
+        for (auto& g : graphM) {
+          leg.AddEntry(&g.second, g.first.c_str(), "pel")->SetTextSize(0.032);
+        }
+        // draw efficiency
+        graph.Draw("ap");
+        
+        const auto& name = effMapTitle[p.first][t.first][v.first];
+        makeDirFile(file,name); 
+        TDirectory* subdir_ = file->GetDirectory(name.c_str());
+        graph.SetName(Form("%s_online",name.c_str()));
+        subdir_->cd();
+        graph.Write();
+	std::vector<string> tags =  {"CPU_Pat","GPU"};
+	int c2 = 0;
+        for (auto& g : graphM) {
+          if (g.first==graphM[0].first) continue;
+          g.second.Draw("samep");
+        g.second.SetName(Form("%s_%s",name.c_str(),tags[c2].c_str()));
+	c2++;
+	std::cout << "TEST_______________________TEST c2-> " <<c2 << std::endl;
+        g.second.Write();
+        }
+
+
+
+        // draw legend
+        leg.Draw("same");
+        // draw line
+        TLine line(graph.GetXaxis()->GetXmin(), 1.0,  graph.GetXaxis()->GetXmax(), 1.0);
+        line.SetLineStyle(2);
+        line.Draw("same");
+        // Update
+        c.Modified(); c.Update();
+        // Draw the text
+        tex.SetTextSize(0.058); tex.SetTextFont(61); tex.DrawLatex(0.78, 0.84, "CMS"); tex.SetTextFont(62);
+        tex.SetTextSize(0.044); tex.SetTextFont(52); tex.DrawLatex(0.69, 0.79, "Preliminary"); tex.SetTextFont(62);
+        for (size_t i=0; i<textToPrint.size(); i++) {
+          tex.SetTextSize(0.030); tex.DrawLatex(0.20, 0.86-i*0.05, textToPrint[i].c_str());
+        }
+        c.Modified(); c.Update();
+        // set the CMS style
+        CMS_lumi(&c, 33, "PbPb run 326381", "#sqrt{s_{NN}} = 5.02 TeV", false, 0.60, false);
+        c.Modified(); c.Update();
+        // Create Output Directory
+        const std::string plotDir = "Plot/" + par;
+        makeDir(plotDir + "/png/");
+        makeDir(plotDir + "/pdf/");
+        // Save Canvas
+        c.SaveAs((plotDir + "/png/" + name + ".png" ).c_str());
+        c.SaveAs((plotDir + "/pdf/" + name + ".pdf" ).c_str());
+        // Clean up memory
+        c.Clear(); c.Close();
+      }
+    }
+  }
+};
